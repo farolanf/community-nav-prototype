0924d96a251771c10cb73af55148f734ef62aca7
Farolan Faisal <farolanfaisal@gmail.com>
2019-05-14 17:17:50 +0800

    Add TopNav

--- tc-nav-react-demo/src/App.js
+++ tc-nav-react-demo/src/App.js
@@ -2,9 +2,154 @@ import React from 'react';
 import { TopNav } from 'tc-nav-react'
 
 function App() {
+  var navMenus = [
+    {
+      title: 'BUSINESS',
+      subMenu: [
+        {
+          title: "Solutions",
+          subMenu: [
+            { title: "All Solutions" },
+            { title: "Apps" },
+            { title: "Websites" },
+            { title: "Product Design" },
+            { title: "Development Tasks" },
+            { title: "Analytics & Data Science" },
+            { title: "Testing & QA" },
+            { title: "How It Works" },
+          ]
+        },
+        {
+          title: "Enterprise Programs",
+          subMenu: [
+            { title: "All Solutions" },
+            { title: "Apps" },
+            { title: "Websites" },
+            { title: "Product Design" },
+            { title: "Development Tasks" },
+            { title: "Analytics & Data Science" },
+            { title: "Testing & QA" },
+            { title: "How It Works" },
+          ]
+        },
+        {
+          title: "Customer Success",
+          subMenu: [
+            { title: "All Solutions" },
+            { title: "Apps" },
+            { title: "Websites" },
+            { title: "Product Design" },
+            { title: "Development Tasks" },
+            { title: "Analytics & Data Science" },
+            { title: "Testing & QA" },
+            { title: "How It Works" },
+          ]
+        },
+        {
+          title: "Company",
+          subMenu: [
+            { title: "All Solutions" },
+            { title: "Apps" },
+            { title: "Websites" },
+            { title: "Product Design" },
+            { title: "Development Tasks" },
+            { title: "Analytics & Data Science" },
+            { title: "Testing & QA" },
+            { title: "How It Works" },
+          ]
+        },
+        {
+          title: "Resources",
+          subMenu: [
+            { title: "All Solutions" },
+            { title: "Apps" },
+            { title: "Websites" },
+            { title: "Product Design" },
+            { title: "Development Tasks" },
+            { title: "Analytics & Data Science" },
+            { title: "Testing & QA" },
+            { title: "How It Works" },
+          ]
+        },
+        {
+          title: "Blog",
+          subMenu: [
+            { title: "All Solutions" },
+            { title: "Apps" },
+            { title: "Websites" },
+            { title: "Product Design" },
+            { title: "Development Tasks" },
+            { title: "Analytics & Data Science" },
+            { title: "Testing & QA" },
+            { title: "How It Works" },
+          ]
+        },
+      ]
+    },
+    {
+      title: 'WORK',
+      subMenu: [
+        {
+          title: "Design",
+          subMenu: [
+            { title: "Overview" },
+            { title: "Work List" },
+            { title: "Stats" },
+            { title: "Problem archive" },
+            { title: "Learn" },
+            { title: "Topcoder Open" },
+          ]
+        },
+        {
+          title: "Development",
+          subMenu: [
+            { title: "Overview" },
+            { title: "Work List" },
+            { title: "Stats" },
+            { title: "Problem archive" },
+            { title: "Learn" },
+            { title: "Topcoder Open" },
+          ]
+        },
+        {
+          title: "Data Science",
+          subMenu: [
+            { title: "Overview" },
+            { title: "Work List" },
+            { title: "Stats" },
+            { title: "Problem archive" },
+            { title: "Learn" },
+            { title: "Topcoder Open" },
+          ]
+        },
+        {
+          title: "QA",
+          subMenu: [
+            { title: "Overview" },
+            { title: "Work List" },
+            { title: "Stats" },
+            { title: "Problem archive" },
+            { title: "Learn" },
+            { title: "Topcoder Open" },
+          ]
+        },
+        {
+          title: "Topcoder Open",
+          subMenu: [
+            { title: "Overview" },
+            { title: "Work List" },
+            { title: "Stats" },
+            { title: "Problem archive" },
+            { title: "Learn" },
+            { title: "Topcoder Open" },
+          ]
+        }
+      ]
+    },
+  ]
   return (
     <div className="App">
-      <TopNav />
+      <TopNav menu={navMenus} />
     </div>
   );
 }
--- tc-nav-react/src/TopNav/index.js
+++ tc-nav-react/src/TopNav/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import 'assets/sass/style.scss'
 
-const TopNav = () => {
+const TopNav = ({ menu }) => {
   return (
     <div className='header-nav-ui'>
       <div className='primary-nav'>
@@ -9,6 +10,16 @@ const TopNav = () => {
       </div>
     </div>
   )
+}
+
+TopNav.propTypes = {
+  /**
+   * Array of menu objects, each with properties:
+   *   - title {string|element} The title
+   *   - href {string} If set, wrap title in <a>
+   *   - subMenu {array} Children menu
+   */
+  menu: PropTypes.array.isRequired
 }
 
 export default TopNav
