6fadb650fd3c85150f175ff803a6ea52e0c66288
Farolan Faisal <farolanfaisal@gmail.com>
2019-05-18 11:34:19 +0800

    Update notifications

--- tc-nav-react-demo/src/App.js
+++ tc-nav-react-demo/src/App.js
@@ -235,6 +235,64 @@ function App() {
     }
   ]
 
+  const notifications = [
+    {
+      content: `Northumbrian Water (NWL) - Customer Engagement Gamification Mobile App Design Concepts Challenge is now open for registrations`,
+      tags: ['Concept Design'],
+      timestamp: Date.now() - 2 * 60 * 60 * 1000
+    },
+    {
+      content: `Eniatus Bank Internal Product Dashboard Design Challenge is now open for registrations`,
+      tags: ['Application Front-End Design'],
+      timestamp: Date.now() - 2.5 * 60 * 60 * 1000
+    },
+    {
+      content: `Your submission ID513A23433-1 is now in review`,
+      category: 'Topcoder Copilot Recruitment Challenge',
+      timestamp: Date.now() - 10 * 60 * 1000,
+    },
+    {
+      content: `Your submission ID513A23433-1 was processed successfully`,
+      category: 'Topcoder Copilot Recruitment Challenge',
+      timestamp: Date.now() - 2.1 * 60 * 60 * 1000,
+    },
+    {
+      content: `Your submission ID513A23433-2 was uploaded successfully`,
+      category: 'Topcoder Copilot Recruitment Challenge',
+      timestamp: Date.now() - 2 * 60 * 60 * 1000,
+    },
+    {
+      content: `Challenge is now in Review phase. No new submissions are accepted at this point.`,
+      category: 'TOSCA Editor - Web Application Wireframe Challenge',
+      timestamp: Date.now() - 60 * 60 * 1000,
+    },
+    {
+      content: `Northumbrian Water (NWL) - Customer Engagement Gamification Mobile App Design Concepts Challenge is now open for registrations`,
+      tags: ['Concept Design'],
+      timestamp: new Date('2019-04-05'),
+    },
+    {
+      content: `FAST 48Hr! Patient Concierge Chatbot Web Application UX Testing Challenge is now open for registrations`,
+      tags: ['Rapid UX'],
+      timestamp: new Date('2019-04-01 10:43'),
+    },
+    {
+      content: `Juno Claims Reporting Tool iOS Mobile Design Application is now open for registrations`,
+      tags: ['Application Front-End Design'],
+      timestamp: new Date('2019-03-28 15:45'),
+    },
+    {
+      content: `Your submission ID513A23433-1 was uploaded successfully`,
+      category: 'Topcoder Copilot Recruitment Challenge',
+      timestamp: new Date('2019-04-10 10:52'),
+    },
+    {
+      content: `You are now registered for Topcoder Copilot Recruitment Challenge`,
+      category: 'Topcoder Copilot Recruitment Challenge',
+      timestamp: new Date('2019-04-08 12:45'),
+    }
+  ]
+
   const [theme, setTheme] = useState('light')
   const [loggedIn, setLoggedIn] = useState()
   const [notificationState, setNotificationState] = useState('new')
@@ -270,6 +328,7 @@ function App() {
             avatarSrc='/img/img-vic-tor-avatar.svg'
             username='vic-tor'
             notificationButtonState={notificationState}
+            notifications={notifications}
             onClickLogin={handleClickLogin}
             accountMenu={[
               { title: 'Settings' },
--- tc-nav-react-demo/src/components/NotificationsPopup/index.js
+++ tc-nav-react-demo/src/components/NotificationsPopup/index.js
@@ -3,25 +3,42 @@ import PropTypes from 'prop-types'
 import cn from 'classnames'
 import styles from './styles.module.scss'
 
-const NotificationsPopup = ({ open, onClose, emptyTitle, emptyText }) => (
-  <div className={cn(styles.notificationsPanel, open && styles.open)}>
-    <div className={styles.backdrop} onClick={onClose} />
-    <div className={styles.notiHeader}>
-      <span className={styles.leftNoti}>Notifications</span>
-      <div className={styles.rights}>
-        <span role='button' className={styles.whiteLink}>Settings</span>
-      </div>
+const NotificationsPopup = ({ open, onClose, emptyTitle, emptyText, notifications }) => {
+  const hasNotifications = notifications && notifications.length
+  const categories = (notifications || []).map(noti => noti.category)
+
+  return (
+    <div className={cn(styles['notifications-panel'], open && styles.open)}>
+      <div className={styles.backdrop} onClick={onClose} />
+
+      {hasNotifications ? (
+        <div>empty</div>
+      ) : (
+        <>
+          <div className={styles['noti-header']}>
+            <span className={styles['notification-back-btn']} role='button' />
+            <span className={styles['left-noti']}>Notifications</span>
+            <div className={styles.rights}>
+              <span className={styles.point}></span>
+              <span className={styles['white-link']} role='button'>Settings</span>
+            </div>
+            <span className={styles['btn-setting']} role='button' />
+          </div>
+          <div className={styles['noti-body']}>
+            <i className={cn(styles.icons, styles['icon-bell'])} />
+            <h4 className={styles.titles}>{emptyTitle}</h4>
+            <div className={styles.txt}>{emptyText}</div>
+          </div>
+          <div className={styles['noti-footer']}>
+            <span className={cn(styles.btn, styles['btn-blue'])} role='button'>
+              Notification Settings
+            </span>
+          </div>
+        </>
+      )}
     </div>
-    <div className={styles.notiBody}>
-      <i className={styles.iconBell} />
-      <h4 className={styles.titles}>{emptyTitle}</h4>
-      <div className={styles.txt}>{emptyText}</div>
-    </div>
-    <div className={styles.notiFooter}>
-      <span role='button' className={cn(styles.btn, styles['btn-blue'])}>Notification Settings</span>
-    </div>
-  </div>
-)
+  )
+}
 
 NotificationsPopup.defaultProps = {
   emptyTitle: 'Good job! You’re all caught up',
